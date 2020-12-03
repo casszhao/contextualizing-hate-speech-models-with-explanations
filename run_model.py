482a34598c7b292ebb69f11ea033278cd42d8d9e
Cass Zhao <cass790412@gmail.com>
2020-12-03 23:36:40 +0000

    add stormfront tsv dataset

--- run_model.py
+++ run_model.py
@@ -1,25 +1,3 @@
-# coding=utf-8
-# Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
-# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""
-Running BERT finetuning & evaluation on hate speech classification datasets.
-
-Integrated with SOC explanation regularization
-
-"""
-
 from __future__ import absolute_import, division, print_function
 
 import argparse
@@ -38,6 +16,11 @@ from torch.utils.data.distributed import DistributedSampler
 from torch import nn
 from torch.nn import functional as F
 from tqdm import tqdm, trange
+
+"""
+Running BERT finetuning & evaluation on hate speech classification datasets.
+Integrated with SOC explanation regularization
+"""
 
 from torch.nn import CrossEntropyLoss, MSELoss
 from scipy.stats import pearsonr, spearmanr
@@ -259,8 +242,7 @@ def main():
     processors = {
         'gab': GabProcessor,
         'ws': WSProcessor,
-        'nyt': NytProcessor,
-        'AG10K': AG10KProcessor
+        'nyt': NytProcessor
     }
 
     output_modes = {
