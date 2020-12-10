997b22663b0fb903b9aaaeb2ebf6623cdca6771b
Cass Zhao <cass790412@gmail.com>
2020-12-10 10:35:28 +0000

    add stormfront tsv dataset

--- data/preprocessor.py
+++ data/preprocessor.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv('./Gab/Gab_validation.csv', usecols=['id', 'comment', 'label'])
+df = pd.read_csv('./Gab/Gab_test.csv', usecols=['id', 'comment', 'label'])
+
+print(df['label'].value_counts())
 
 df.columns = ['doc_id','text','is_hate']
-df.to_csv('./Gab/dev.tsv', sep='\t', index=False)
+df.to_csv('./Gab/test.tsv', sep='\t', index=False)
 
 print(df.head(3))
