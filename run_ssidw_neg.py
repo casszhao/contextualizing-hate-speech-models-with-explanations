950599dedcad1e58fc42218b5ec92c48f330d723
Cass Zhao <cass790412@gmail.com>
2020-12-30 13:21:59 +0000

    add stormfront tsv dataset

--- run_ssidw_neg.py
+++ run_ssidw_neg.py
@@ -27,7 +27,7 @@ from sklearn.metrics import matthews_corrcoef, f1_score
 from sklearn.metrics import precision_score, recall_score, roc_auc_score
 
 from bert.file_utils import PYTORCH_PRETRAINED_BERT_CACHE, WEIGHTS_NAME, CONFIG_NAME
-from bert.modeling import BertForSequenceClassification, BertConfig, BertForSequenceClassification_Ss, BertForSequenceClassification_Ss_IDW, BertForSequenceClassification_Ss_IDW_neg
+from bert.modeling import BertForSequenceClassification, BertConfig, BertForSequenceClassification_Ss_IDW_multiply
 from bert.tokenization import BertTokenizer
 from bert.optimization import BertAdam, WarmupLinearSchedule
 
