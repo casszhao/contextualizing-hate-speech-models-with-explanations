696129bbf54c1636182089de00e6a91577884a59
Cass Zhao <cass790412@gmail.com>
2021-01-01 10:45:47 +0000

    add stormfront tsv dataset

--- bert/modeling.py
+++ bert/modeling.py
@@ -1021,6 +1021,58 @@ class BertForSequenceClassification_Ss_IDW(BertPreTrainedModel):
             return logits
 
 
+
+class BertForSequenceClassification_Ss_IDW_weight(BertPreTrainedModel):
+
+    def __init__(self, config, num_labels=None, tokenizer=None):
+        super(BertForSequenceClassification_Ss_IDW_weight, self).__init__(config)
+        self.num_labels = num_labels
+        self.tokenizer = tokenizer
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size+2, num_labels)
+        self.apply(self.init_bert_weights)
+
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None, tokenizer=None, device=None,
+                class_weight=None):
+        _, pooled_output = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
+        pooled_output = self.dropout(pooled_output)
+
+        # tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=True)
+
+        inputids_first_dimension = input_ids.size()[0]
+        Ss = torch.empty(inputids_first_dimension, 1).to(device)
+        IDW = torch.empty(inputids_first_dimension, 1).to(device)
+        for i, the_id in enumerate(input_ids):
+            sent = tokenizer.convert_ids_to_tokens(the_id.tolist())
+            new_sent = ''
+            for word in sent:
+                if word != '[PAD]':
+                    new_sent = new_sent + word + ' '
+
+            blob = TextBlob(new_sent)
+            subjective = blob.sentiment.subjectivity
+            Ss[i, 0] = subjective
+
+            sent = [x.lower() for x in sent]
+            words = set(sent)
+            inter = words.intersection(igw)
+            if len(inter) > 0:
+                IDW[i, 0] = 1
+            elif len(inter) == 0:
+                IDW[i, 0] = 0
+
+        pooled_output = torch.cat([pooled_output, Ss, IDW], dim=1)
+        logits = self.classifier(pooled_output)
+
+        if labels is not None:
+            loss_fct = CrossEntropyLoss(class_weight)
+            loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            return loss
+        else:
+            return logits
+
+
 '''
 128*Ss
 positive loss for IDW
