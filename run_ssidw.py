a61470117bc86ee76044b9cf18d07ef5df89c4ed
Cass Zhao <cass790412@gmail.com>
2021-01-25 18:07:03 +0000

    add stormfront tsv dataset

--- run_ssidw.py
+++ run_ssidw.py
@@ -548,8 +548,9 @@ def main():
 
     if args.do_eval and (args.local_rank == -1 or torch.distributed.get_rank() == 0):
         if not args.explain:
+            args.test = True
             validate(args, model, processor, tokenizer, output_mode, label_list, device, num_labels,
-                     task_name, tr_loss, global_step=0, epoch=-1, explainer=explainer, args.test)
+                     task_name, tr_loss, global_step=0, epoch=-1, explainer=explainer)
         else:
             explain(args, model, processor, tokenizer, output_mode, label_list, device)
 
