6e506808382b5697316e7e72169ec8e5d70ab3ff
Cass Zhao <cass790412@gmail.com>
2021-01-25 11:46:58 +0000

    add stormfront tsv dataset

--- bert/modeling.py
+++ bert/modeling.py
@@ -917,117 +917,69 @@ class BertForNextSentencePrediction(BertPreTrainedModel):
 
 class BertForSequenceClassification(BertPreTrainedModel):
 
-    def __init__(self, config, num_labels, add_pooling_layer=True):
+    def __init__(self, config, num_labels):
         super(BertForSequenceClassification, self).__init__(config)
         self.num_labels = num_labels
+
 
         #self.bert = BertModel(config)
         self.embeddings = BertEmbeddings(config)
         self.encoder = BertEncoder(config)
-        self.pooler = BertPooler(config) if add_pooling_layer else None
+        self.pooler = BertPooler(config)
+
 
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, num_labels)
         self.apply(self.init_bert_weights)
 
-    def get_input_embeddings(self):
-        return self.embeddings.word_embeddings
-
-    def set_input_embeddings(self, value):
-        self.embeddings.word_embeddings = value
-
-    def _prune_heads(self, heads_to_prune):
-        for layer, heads in heads_to_prune.items():
-            self.encoder.layer[layer].attention.prune_heads(heads)
-
     def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None,
         position_ids=None,
-        head_mask=None,
-        inputs_embeds=None,
-        encoder_hidden_states=None,
+        #head_mask=None,
+        #inputs_embeds=None,
+        #encoder_hidden_states=None,
         encoder_attention_mask=None,
-        past_key_values=None,
+        #past_key_values=None,
         use_cache=None,
         #output_attentions=None,
         #output_hidden_states=None,
         #return_dict=None,
+        output_all_encoded_layers=True,
                 ):
 
-        #output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
-        #output_hidden_states = (
-        #    output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
-        #)
-        #return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
-        #if self.config.is_decoder:
-        #    use_cache = use_cache if use_cache is not None else self.config.use_cache
-        #else:
-        use_cache = False
-
-        if input_ids is not None and inputs_embeds is not None:
-            raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
-        elif input_ids is not None:
-            input_shape = input_ids.size()
-            batch_size, seq_length = input_shape
-        elif inputs_embeds is not None:
-            input_shape = inputs_embeds.size()[:-1]
-            batch_size, seq_length = input_shape
-        else:
-            raise ValueError("You have to specify either input_ids or inputs_embeds")
-
-        device = input_ids.device if input_ids is not None else inputs_embeds.device
-
-        past_key_values_length = past_key_values[0][0].shape[2] if past_key_values is not None else 0
-
         if attention_mask is None:
-            attention_mask = torch.ones(((batch_size, seq_length + past_key_values_length)), device=device)
+            attention_mask = torch.ones_like(input_ids)
         if token_type_ids is None:
-            token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=device)
+            token_type_ids = torch.zeros_like(input_ids)
 
-        # We can provide a self-attention mask of dimensions [batch_size, from_seq_length, to_seq_length]
-        # ourselves in which case we just need to make it broadcastable to all heads.
-        extended_attention_mask: torch.Tensor = self.get_extended_attention_mask(attention_mask, input_shape, device)
+            # We create a 3D attention mask from a 2D tensor mask.
+            # Sizes are [batch_size, 1, 1, to_seq_length]
+            # So we can broadcast to [batch_size, num_heads, from_seq_length, to_seq_length]
+            # this attention mask is more simple than the triangular masking of causal attention
+            # used in OpenAI GPT, we just need to prepare the broadcast dimension here.
+        extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
 
-        if self.config.is_decoder and encoder_hidden_states is not None:
-            encoder_batch_size, encoder_sequence_length, _ = encoder_hidden_states.size()
-            encoder_hidden_shape = (encoder_batch_size, encoder_sequence_length)
-            if encoder_attention_mask is None:
-                encoder_attention_mask = torch.ones(encoder_hidden_shape, device=device)
-            encoder_extended_attention_mask = self.invert_attention_mask(encoder_attention_mask)
-        else:
-            encoder_extended_attention_mask = None
-
-            # Prepare head mask if needed
-            # 1.0 in head_mask indicate we keep the head
-            # attention_probs has shape bsz x n_heads x N x N
-            # input head_mask has shape [num_heads] or [num_hidden_layers x num_heads]
-            # and head_mask is converted to shape [num_hidden_layers x batch x num_heads x seq_length x seq_length]
-        head_mask = self.get_head_mask(head_mask, self.config.num_hidden_layers)
+        # Since attention_mask is 1.0 for positions we want to attend and 0.0 for
+        # masked positions, this operation will create a tensor which is 0.0 for
+        # positions we want to attend and -10000.0 for masked positions.
+        # Since we are adding it to the raw scores before the softmax, this is
+        # effectively the same as removing these entirely.
+        extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype)  # fp16 compatibility
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
 
         #_, pooled_output = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
-        embedding_output = self.embeddings(
-            input_ids=input_ids,
-            #position_ids=position_ids,
-            token_type_ids=token_type_ids,
-            #inputs_embeds=inputs_embeds,
-            #past_key_values_length=past_key_values_length,
-        )
+        if len(input_ids.size()) == 2:
+            embedding_output = self.embeddings(input_ids, token_type_ids)
+        else:
+            embedding_output = input_ids
+        print('embedding outputs size: ', embedding_output.size())
         encoder_outputs = self.encoder(
             embedding_output,
             attention_mask=extended_attention_mask,
-            head_mask=head_mask,
-            encoder_hidden_states=encoder_hidden_states,
-            encoder_attention_mask=encoder_extended_attention_mask,
-            past_key_values=past_key_values,
-            use_cache=use_cache,
-            #output_attentions=output_attentions,
-            #output_hidden_states=output_hidden_states,
-            #return_dict=return_dict,
+            output_all_encoded_layers=output_all_encoded_layers,
         )
         print('encoder outputs size: ', encoder_outputs.size())
-        sequence_output = encoder_outputs[0]
+        sequence_output = encoder_outputs[-1]
         pooled_output = self.pooler(sequence_output) if self.pooler is not None else None
-
 
 
         pooled_output = self.dropout(pooled_output)
