03af3c3b51c522300e376c6eba2377f9bbc60fdf
Cass Zhao <cass790412@gmail.com>
2021-01-26 10:33:52 +0000

    add stormfront tsv dataset

--- evaluate_results.py
+++ evaluate_results.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import numpy as np
 import re
+from sklearn.metrics import confusion_matrix, classification_report, f1_score
 
-path = './data/eval_details_400_dev_ws.txt'
-new_csv_name = './data/eval_details_400_dev_ws.csv'
+path = './results/eval_details_0_test_ws.txt'
+new_csv_name = './results/ws_bert.csv'
 def csv2txt(path, new_csv_name):
 
     with open(path, 'r') as f:
@@ -34,5 +36,27 @@ def csv2txt(path, new_csv_name):
     # print(df.head(5))
 
     df.to_csv(new_csv_name, sep=',', index=False)
+    return df
 
-csv2txt(path, new_csv_name)
+ws_bert = csv2txt(path, new_csv_name)
+
+conditions = [ ws_bert['pre_non'] > ws_bert['pre_toxic'], ws_bert['pre_non'] < ws_bert['pre_toxic'] ]
+choices = [0, 1]
+ws_bert['prediction'] = np.select(conditions, choices, default=np.nan)
+
+confusion_matrix = confusion_matrix(ws_bert['label'], ws_bert['prediction'])
+classification_report = classification_report(ws_bert['label'], ws_bert['prediction'])
+
+TN = confusion_matrix[0][0]
+FN = confusion_matrix[1][0]
+TP = confusion_matrix[1][1]
+FP = confusion_matrix[0][1]
+
+print(TN)
+print(FN)
+print(TP)
+print(FP)
+
+
+print(classification_report)
+print(f1_score(ws_bert['label'], ws_bert['prediction']))
