47a8c69363416c4876ec8226d9792684e45ea0e1
Cass Zhao <cass790412@gmail.com>
2021-02-02 16:09:00 +0000

    add stormfront tsv dataset

--- plot.py
+++ plot.py
@@ -3,15 +3,57 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from textblob import TextBlob
 import nltk
+import re
 nltk.download('averaged_perceptron_tagger')
 
 
-data_path = './results/ws_bert.csv'
+def txt2csv(path, new_csv_name):
+
+    with open(path, 'r') as f:
+        content = f.readlines()
+
+    label = []
+    prediction = []
+    pre_non = []
+    pre_toxic = []
+    text = []
+
+    for line in content:
+        l, pl, pre, t = line.strip().split('\t')
+        number = re.findall(r"\d+\.?\d*",pre)
+        label.append(int(l))
+        prediction.append(int(pl))
+        pre_non.append(float(number[0]))
+        pre_toxic.append(float(number[1]))
+
+        clean_txt = ''
+        word_list = t.split(' ')
+        for word in word_list[1:-1]:
+            clean_txt = clean_txt + word + ' '
+
+        text.append(clean_txt)
+
+    df = pd.DataFrame({
+        "label": label,
+        "prediction": prediction,
+        "pre_non": pre_non,
+        "pre_toxic": pre_toxic,
+        "text": text
+    })
+
+
+    # print(df.head(5))
+
+    df.to_csv(new_csv_name, sep=',', index=False)
+    return df
+
+
+
 def addinfo(data_name, data_path):
     print(data_name)
-    df = pd.read_csv(data_path, usecols=['text','label'] ) #, nrows=200
+    df = pd.read_csv(data_path, usecols=['text','label','prediction'] ) #, nrows=200
     # make pesudo prediction
-    df['Prediction'] = 1
+    #df['Prediction'] = 1
     #df['text'] = df['text'].map(lambda x: x.lstrip('[CLS]',).rstrip('aAbBcC'))
     #df['text'] = df['text'].map(lambda x: x.lstrip('[SEP]',).rstrip('aAbBcC'))
     #df['text'] = df['text'].str.replace(r'[CLS]', '').str.replace(r'[SEP]', '')
@@ -32,13 +74,13 @@ def addinfo(data_name, data_path):
 
 
     def results_tpye(row):
-        if (row['label'] == 1 and row['Prediction'] == 1):
+        if (row['label'] == 1 and row['prediction'] == 1):
             return 'True Positive'
-        if row['label'] == 0 and row['Prediction'] == 1:
+        if row['label'] == 0 and row['prediction'] == 1:
             return 'False Positive'
-        if row['label'] == 0 and row['Prediction'] == 0:
+        if row['label'] == 0 and row['prediction'] == 0:
             return 'True Negative'
-        if row['label'] == 1 and row['Prediction'] == 0:
+        if row['label'] == 1 and row['prediction'] == 0:
             return 'False Negative'
         return 'Other'
     df['Result'] = df.apply(lambda row: results_tpye(row), axis=1)
@@ -49,13 +91,27 @@ def addinfo(data_name, data_path):
     # only keep 'True Positive' and 'False Positive'
     return df
 
+def combine_all_process(data_name, txt_path, csv_path):
+    df = txt2csv(txt_path, csv_path)
+    df_Ss = addinfo(data_name, csv_path)
+    return df_Ss
+
+D1 = combine_all_process('ws', './results/ws_bert.txt', './results/ws_bert.csv')
+D2 = combine_all_process('wassem', './results/wassem_bert.txt', './results/wassem_bert.csv')
+D3 = combine_all_process('AG10K', './results/AG10K_bert.txt', './results/AG10K_bert.csv')
+D4 = combine_all_process('tweet50k', './results/tweet50k_bert.txt', './results/tweet50k_bert.csv')
+D5 = combine_all_process('mt', './results/mt_bert.txt', './results/mt_bert.csv')
+frames = [D1, D2, D3, D4, D5]
+# D1_df = txt2csv('./results/ws_bert.txt', './results/ws_bert.csv')
+# D1_df_Ss = addinfo('D1', './results/ws_bert.csv')
+
+# D2_df = txt2csv('./results/ws_bert.txt', './results/ws_bert.csv')
+# D2_df_Ss = addinfo('D2', './results/ws_bert.csv')
+
+# D3_df = txt2csv('./results/ws_bert.txt', './results/ws_bert.csv')
+# D3_df_Ss = addinfo('D3', './results/ws_bert.csv')
 
 
-D1 = addinfo('D1', data_path)
-D2 = addinfo('D2', data_path)
-D3 = addinfo('D3', data_path)
-
-frames = [D1, D2, D3]
 # label, text, Prediction, Subjective Score, Data, Result
 df = pd.concat(frames)
 print(df)
