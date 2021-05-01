21bcbddf55a6efabdca1f5de8b1a6ba36fa352bd
Cass Zhao <cass790412@gmail.com>
2021-05-01 21:28:35 +0100

    modify segment id for roberta

--- run_ss-roberta.py
+++ run_ss-roberta.py
@@ -374,13 +374,13 @@ def main():
     cache_dir = args.cache_dir if args.cache_dir else os.path.join(str(PYTORCH_PRETRAINED_BERT_CACHE),
                                                                    'distributed_{}'.format(args.local_rank))
     if args.do_train:
-        model = RobertaForSequenceClassification.from_pretrained(args.bert_model,
+        model = RobertaForSequenceClassification_Ss_IDW.from_pretrained(args.bert_model,
                                                               cache_dir=cache_dir,
                                                               num_labels=num_labels)
 
 
     else:
-        model = RobertaForSequenceClassification.from_pretrained(args.output_dir, num_labels=num_labels)
+        model = RobertaForSequenceClassification_Ss_IDW.from_pretrained(args.output_dir, num_labels=num_labels)
     model.to(device)
 
     if args.fp16:
