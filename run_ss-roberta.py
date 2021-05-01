5f436f12791d6ade270edc5821baeff8665f632b
Cass Zhao <cass790412@gmail.com>
2021-05-01 22:12:37 +0100

    modify segment id for roberta

--- run_ss-roberta.py
+++ run_ss-roberta.py
@@ -33,8 +33,8 @@ from bert.tokenization import BertTokenizer
 from bert.optimization import BertAdam, WarmupLinearSchedule
 from bert.ss_roberta_model import RobertaForSequenceClassification_Ss_IDW
 
-from transformers import RobertaModel, RobertaConfig, RobertaTokenizer, PreTrainedModel#RobertaPreTrainedModel
-from transformers import RobertaForSequenceClassification
+from transformers import RobertaTokenizer#RobertaPreTrainedModel, RobertaModel, RobertaConfig, PreTrainedModel
+#from transformers import RobertaForSequenceClassification
 
 from loader import GabProcessor, WSProcessor, NytProcessor, convert_examples_to_features #,multiclass_Processor,multilabel_Processor
 from utils.config import configs, combine_args
